68a4f1a9d71141d28b5827620009aa0398786364
Niklas Adolfsson <niklasadolfsson1@gmail.com>
2025-01-17 18:14:38 +0100

    remove noise

--- tests/wasm.rs
+++ tests/wasm.rs
@@ -25,7 +25,6 @@ async fn connect_chainhead() -> OnlineClient<PolkadotConfig> {
     let config = ChainConfig::chain_spec(include_str!("../local.json"))
         .set_bootnodes([
             "/ip4/127.0.0.1/tcp/30333/ws/p2p/12D3KooWEyoppNCUx8Yx66oV9fJnriXwCcXwDDUA2kj6vnc6iDEp",
-            "/dns/localhost/tcp/30333/ws/p2p/12D3KooWEyoppNCUx8Yx66oV9fJnriXwCcXwDDUA2kj6vnc6iDEp",
         ])
         .unwrap();
     let (_client, rpc) = LightClient::relay_chain(config).unwrap();
@@ -43,31 +42,6 @@ async fn testing() {
     console_error_panic_hook::set_once();
     tracing_wasm::set_as_global_default();
 
-    // Start substrate node alice:
-    //
-    // $ /target/release/substrate-node \
-    // --base-path /tmp/alice \
-    // --chain local \
-    // --alice \
-    // --port 30333 \
-    // --node-key 0000000000000000000000000000000000000000000000000000000000000001 \
-    // --telemetry-url "wss://telemetry.polkadot.io/submit/ 0" \
-    // --validator
-
-    // Start substrate node bob:
-    //
-    // $ ./target/release/substrate-node \
-    // --base-path /tmp/bob \
-    // --chain local \
-    // --bob \
-    // --port 30334 \
-    // --rpc-port 9945 \
-    // --node-key 0000000000000000000000000000000000000000000000000000000000000002 \
-    // --telemetry-url "wss://telemetry.polkadot.io/submit/ 0" \
-    // --validator
-
     // Connect to the chain.
     connect_chainhead().await;
-
-    assert_eq!(1, 0);
 }
